ed1b53d4f65cb12cb2244c98a9aa08965642aefe
Oleh <endore8@gmail.com>
2024-08-31 16:59:20 +0200

    Created menu bar app #1

---
+++ Revenue Bar/Sources/App/RevenueBarApp.swift
@@ -0,0 +1,18 @@
+//
+//  RevenueBarApp.swift
+//  Revenue Bar
+//
+//  Created by Oleh on 31/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct RevenueBarApp: App {
+    
+    var body: some Scene {
+        MenuBarExtra("Utility App", systemImage: "hammer") {
+            ContentView()
+        }
+    }
+}
---
+++ Revenue Bar/Sources/Modules/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Revenue Bar
+//
+//  Created by Oleh on 31/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
