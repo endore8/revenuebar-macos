5be193f0e74e30b5a47a405d390060e6b335482b
Oleh <endore8@gmail.com>
2024-09-01 15:18:13 +0200

    Defined Project model #1

---
+++ Revenue Bar/Sources/Tools/Domain/Model/Project.swift
@@ -0,0 +1,13 @@
+//
+//  Project.swift
+//  Revenue Bar
+//
+//  Created by Oleh on 01/09/2024.
+//
+
+import Foundation
+
+struct Project: Codable, Hashable {
+    let key: String
+    let name: String
+}
